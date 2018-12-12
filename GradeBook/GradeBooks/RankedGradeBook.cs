a0b3d9c0788db2289609767cb2542403a0ef9ec6
Efren Espana Lopez <eelespanish@gmail.com>
2018-12-12 14:46:42 +1000

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -27,7 +27,7 @@ namespace GradeBook.GradeBooks
             else if (grades[(threshold * 4) - 1] <= averageGrade)
                 return 'D';
             else
-                return 'F';   
+                return 'F';
         }
     }
 }
